e0255944a0a872501fc524d00c8dc8cca7fc3691
sebbai <bailly93@gmail.com>
2023-06-14 13:34:48 +0200

    [UPDATE] Test

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,107 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "./assets/images/slideshow/slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "./assets/images/slideshow/slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "./assets/images/slideshow/slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "./assets/images/slideshow/slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const arrowLeft = document.getElementsByClassName('arrow_left');
+arrowLeft[0].addEventListener("click", function () {
+	console.log("Click on left");
+});
+
+const arrowRight = document.getElementsByClassName('arrow_right');
+arrowRight[0].addEventListener("click", function () {
+	console.log("Click on right");
+});
+
+numberOfslides = slides.length;
+console.log("Il y a " + numberOfslides + " element dans le tab slide");
+
+
+for (let index = 0; index < numberOfslides; index++) {
+	let slide = slides[index];
+	let divDot = document.createElement('div');
+	divDot.classList.add('dot');
+	const dotContainer = document.querySelector('.dots');
+	dotContainer.appendChild(divDot);
+
+	let divImage = document.createElement('img');
+	divImage.classList.add('banner-img');
+	divImage.src = slide.image;
+	let imgContainer = document.querySelector('#banner')
+	imgContainer.appendChild(divImage);
+
+
+
+
+
+	/*	
+		let divTagLine = document.createElement('p');
+		divTagLine.innerHTML = slide.tagLine;
+		let textContainer = document.querySelector('#banner')
+		textContainer.appendChild(divTagLine);
+	*/
+}
+
+initializeDot();
+
+function initializeDot() {
+
+
+	let dotsArray = document.querySelectorAll('div.dot')
+	for (i = 0; i < dotsArray.length; i++) {
+		console.log("Dot n': " + i + "");
+
+		if (i === 0) {
+			dotsArray[i].addEventListener("click", selectDot(dotsArray[i]));
+			console.log("IF");
+		}
+		else {
+			dotsArray[i].addEventListener("click", deleteDotSelected(dotsArray[i]));
+			console.log("ELSE");
+		}
+	}
+
+	function selectDot(dotSelected) {
+		dotSelected.classList.toggle('dot_selected');
+	}
+
+	function deleteDotSelected(dotUnselected) {
+		dotUnselected.classList.remove('dot_selected');
+	}
+}
+
+
+let dotsContainer = document.querySelector('.dots');
+
+dotsContainer.addEventListener('click', function () {
+	let dotSelected = document.querySelector('.dot');
+	dotSelected.classList.toggle('dot_selected');
+})
+
+
+
+
+
+/*
+slides.forEach(image) => {
+	let divDot = document.createElement('div');
+	divDot.src = image.image;
+	divDot.classList.add('dot');
+	const dotContainer = document.querySelector('.dots');
+	dotContainer.appendChild(divDot);
+}
+*/
